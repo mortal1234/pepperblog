03e026b8b1b03634a12c04bcb417ba4c5b547d82
William Reid Pybus <willpybus4@gmail.com>
2023-07-01 17:13:26 -0500

    new page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Pepper Site' });
 });
-
-module.exports = router;
